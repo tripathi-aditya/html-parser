44347260a93e530ccdba75e2faf1a2a24d0a4871
tripathi-aditya <adityaomail@gmail.com>
2024-05-08 13:13:29 +0530

    refactor board logic

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
-let isCurrentX = true;
-let cellsByMark = [null, null, null, null, null, null, null, null, null]
-let isWinner = null;
 
-function playTurn(e) {
-    const playMark = isCurrentX ? document.createTextNode('X') : document.createTextNode('O');
-    cellsByMark[e.currentTarget.getAttribute('data-cell-id')] = isCurrentX ? 'X' : 'O'
-    e.currentTarget.appendChild(playMark);
-    isWinner = checkWinner(isCurrentX);
-    if(isWinner){
-        let winner = document.querySelector(`[data-result="${isCurrentX ? 'X' : 'O'}"]`)
-        winner.appendChild(document.createTextNode('Winner!'))
-    }
-    isCurrentX = !isCurrentX;
+let board = [null, null, null, null, null, null, null, null, null]
+
+const players = [{name: "", symbol: "X"}, {name: "", symbol: "O"}];
+
+let currentPlayer = "X";  //symbol
+
+
+function toggleCurrentPlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
 
-const allEqual = (arr) => arr.every(val => val == arr[0] && val != null)
 
-function checkWinner(isX){
+function renderBoardState(){
+    board.forEach((cellValue, index) => {
+        if(cellValue){
+            document.querySelector(`[data-cell-id="${index}"]`).innerHTML = cellValue
+        }
+    });
+}
+
+function updateBoardState(e){
+    const baordCellIndex = e.target.getAttribute('data-cell-id')
+    board[baordCellIndex] = currentPlayer;
+}
+
+
+function handleBoardClick(e){
+    updateBoardState(e);
+    renderBoardState();
+    const winner = checkWinner(currentPlayer)
+    if(winner) document.dispatchEvent(new CustomEvent("gameEnd", {detail: {isDraw: false, winner}}))
+    if(!board.some(turn => turn === null)) document.dispatchEvent(new CustomEvent("gameEnd", {detail: {isDraw: true}}))
+    toggleCurrentPlayer();
+}
+
+
+function handleResults(e){
+    const {winner} = e.detail;
+    let winnerElement = document.querySelector(`[data-result="${winner}"]`)
+    winnerElement.appendChild(document.createTextNode('Winner!'))
+    document.getElementById('board').removeEventListener("click", handleBoardClick);
+
+}
+
+document.getElementById('board').addEventListener('click', handleBoardClick);
+document.addEventListener("gameEnd", handleResults)
+
+
+
+
+function checkWinner(currentPlayer){
+    const allEqual = (arr) => arr.every(val => val == arr[0] && val != null)
     if(
-    allEqual([cellsByMark[0], cellsByMark[1], cellsByMark[2]])
-    || allEqual([cellsByMark[0], cellsByMark[4], cellsByMark[8]])
-    || allEqual([cellsByMark[0], cellsByMark[3], cellsByMark[6]])
-    || allEqual([cellsByMark[1], cellsByMark[4], cellsByMark[7]])
-    || allEqual([cellsByMark[2], cellsByMark[4], cellsByMark[6]])
-    || allEqual([cellsByMark[2], cellsByMark[5], cellsByMark[8]])
-    || allEqual([cellsByMark[3], cellsByMark[4], cellsByMark[5]])
-    || allEqual([cellsByMark[6], cellsByMark[7], cellsByMark[8]])
+    allEqual([board[0], board[1], board[2]])
+    || allEqual([board[0], board[4], board[8]])
+    || allEqual([board[0], board[3], board[6]])
+    || allEqual([board[1], board[4], board[7]])
+    || allEqual([board[2], board[4], board[6]])
+    || allEqual([board[2], board[5], board[8]])
+    || allEqual([board[3], board[4], board[5]])
+    || allEqual([board[6], board[7], board[8]])
     ){
-        return isX ? 'X' : 'O';
+        return currentPlayer;
     }
     return null;
 }
-
-function initialise() {
-    const cells = document.getElementsByClassName('cell');
-
-    for(let i = 0; i < cells.length; i++){
-        cells.item(i).addEventListener('click', playTurn, {once: 'true'})
-    }
-}
-
-
-document.addEventListener('DOMContentLoaded', initialise)
